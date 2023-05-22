28050e7c01e6a0170c2ef41054884cdfd22a7ec7
zwpseudo <zackrey@mail.com>
2023-05-22 00:20:07 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'watford.cloud',
+    description: 'The official store for workspaces used in watford.cloud.',
+    icon: ' https://zwpseudo.github.io/pseudo-registry/1.0/default.png',
+    listUrl: 'https://registry.watford.cloud/',
+    contactUrl: 'https://watford.cloud',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/pseudo-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
