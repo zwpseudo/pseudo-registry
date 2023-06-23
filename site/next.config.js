96350b8387b643070f438c3d94d5bcfc401c16cd
zwpseudo <zackrey@mail.com>
2023-06-23 14:26:13 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,13 +4,13 @@ const nextConfig = {
   env: {
     name: 'watford.cloud',
     description: 'The official store for workspaces used in watford.cloud.',
-    icon: 'https://registry.watford.cloud/1.0/default.png',
-    listUrl: 'https://registry.watford.cloud',
+    icon: 'https://zwpseudo.github.io/pseudo-registry/1.0/default.png',
+    listUrl: 'https://zwpseudo.github.io/pseudo-registry/',
     contactUrl: 'https://watford.cloud',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/1.0',
+  basePath: '/pseudo-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
