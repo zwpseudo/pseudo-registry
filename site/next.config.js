49ca60b4f18b1a6b6f144ed3235477d627a78e60
zwpseudo <zackrey@mail.com>
2023-06-23 14:42:55 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/1.0',
+  basePath: '/pseudo-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
